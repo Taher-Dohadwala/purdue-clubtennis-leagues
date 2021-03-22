78d7289aa4fdd343840274941e2cebc102080289
Taher Dohadwala <taher.dohadwala1@gmail.com>
2021-03-22 09:01:03 -0400

    added option for first week matches

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from dataloader import update_main_and_clear_sr
 from Match_Making import assign_matches_and_update_weeklymatches
+import sys
 
 if __name__ == "__main__":
-    update_main_and_clear_sr()
-    assign_matches_and_update_weeklymatches()
+    # First week of leagues
+    if sys.argv[1] == "start":
+        assign_matches_and_update_weeklymatches()
+    # all other weeks of leagues
+    else:
+        update_main_and_clear_sr()
+        assign_matches_and_update_weeklymatches()
     print("Finished")
